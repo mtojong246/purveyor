5b000fd4221a99381811fd858601d698ea93108c
Melanie Tojong <mtojong@Melanies-MacBook-Pro.local>
2023-08-04 15:32:05 -0400

    creating navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,51 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import User from './assets/user.svg';
+import Cart from './assets/cart.svg';
+import Search from './assets/search.svg';
+import Close from './assets/close.svg';
+import Menu from './assets/menu.svg';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='w-screen'>
+      <div className='flex justify-between items-center bg-primary-color p-5 border-b border-[#474747]'>
+        <div className='flex justify-between items-center gap-5'>
+          <button>
+            <img src={Menu} alt='menu' className='block xl:hidden w-[35px] ' />
+          </button>
+          <img src='/purveyor_logo.png' alt='logo' className='h-[35px]'/>
+        </div>
+        <div className='hidden xl:block'>
+          <button className='border-r border-white text-white text-sm pr-2 mr-2'>Shop</button>
+          <button className='border-r border-white text-white text-sm pr-2 mr-2'>About</button>
+          <button className='border-r border-white text-white text-sm pr-2 mr-2'>Knife Makers</button>
+          <button className='border-r border-white text-white text-sm pr-2 mr-2'>Sell</button>
+          <button className='border-white text-white text-sm'>Contact</button>
+        </div>
+        <div className='flex xl:hidden justify-center items-center gap-4'>
+            <img src={User} alt='user' className='w-[28px]'/>
+            <img src={Cart} alt='cart' className='w-[28px]' />
+        </div>
+      </div>
+      <div className='flex justify-between items-center bg-primary-color p-5'>
+          <div className='hidden xl:flex justify-between items-center gap-[30px]'>
+            <button className='text-white'>NEW KNIVES</button>
+            <button className='text-white'>EVERYDAY CARRY</button>
+            <button className='text-white'>COLLECTOR GRADE</button>
+            <button className='text-white'>INVESTMENT QUALITY</button>
+            <button className='text-white'>FIXED BLADES</button>
+          </div>
+          <div className='flex justify-end xl:justify-between items-center gap-4 w-full xl:w-fit'>
+            <div className='grow bg-[#D9D9D9] rounded-lg py-1 px-2 flex justify-between items-center'>
+              <img src={Search} alt='search' className='w-[20px]' />
+              <input className='grow px-2 bg-[#D9D9D9]' placeholder='Search' />
+              <img src={Close} alt='close' className='w-[20px]' /> 
+            </div>
+            <img src={User} alt='user' className='w-[28px] hidden xl:block'/>
+            <img src={Cart} alt='cart' className='w-[28px] hidden xl:block' />
+          </div>
+      </div>
+      
     </div>
   );
 }
---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        "primary-color": "var(--primary-color)",
+        "secondary-color": "var(--secondary-color)",
+        "tertiary-color": "var(--tertiary-color)",
+      },
+      fontSize: {
+        xsm: ['12px', '18px'],
+        sm: ['14px', '20px'],
+        base: ['16px', '24px'],
+        lg: ['20px', '28px'],
+        xl: ['24px', '32px'],
+        '2xl': ['30px', '38px'],
+        '3xl': ['40px', '48px'],
+      }
+    },
+  },
+  plugins: [],
+}
